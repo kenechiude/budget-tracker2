83877fc4039b5fe5aa6aa827696f02bbfb01c5d2
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-18 13:40:46 +0100

    Updated IncomeList.js

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -1,16 +1,22 @@
 export default function IncomeList({ incData }) {
-  // console.log(incData);
+  const formarter = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    maximumFractionDigits: 2,
+  });
 
   return (
     <div className="income-list">
-      <span>Income</span>
+      <span className="inc">Income</span>
       <ul>
-        {incData.map((data) => (
-          <li key={data.id}>
-            <span>{data.des}</span>
-            <span>{data.amt}</span>
-          </li>
-        ))}
+        {incData.map(
+          (data) =>
+            data.amt && (
+              <li className="inc-li" key={data.id}>
+                <span>{data.des}</span>
+                <span>{formarter.format(data.amt)}</span>
+              </li>
+            )
+        )}
       </ul>
     </div>
   );
