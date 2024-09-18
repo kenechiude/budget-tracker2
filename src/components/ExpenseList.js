c9d64f4e96fb672c4399ad6a60ca672fe75be2f4
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-18 13:40:00 +0100

    Updated ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,22 +1,30 @@
 export default function ExpenseList({ expData }) {
-  // console.log(expData);
   const expense = expData.reduce((acc, obj) => (acc += obj.amt), 0);
+  const formarter = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    maximumFractionDigits: 2,
+  });
+
   function calcPerc(amt) {
     return Math.round((amt / expense) * 100);
   }
 
   return (
     <div className="expense-list">
-      <span>Expense</span>
+      <span className="exp">Expense</span>
       <ul>
-        {expData.map((data) => (
-          <li key={data.id}>
-            <span>{data.des}</span>
-            <span>
-              {data.amt} <span className="perc">{calcPerc(data.amt)}%</span>
-            </span>
-          </li>
-        ))}
+        {expData.map(
+          (data) =>
+            data.amt && (
+              <li className="exp-li" key={data.id}>
+                <span>{data.des}</span>
+                <span className="ex-spn">
+                  {formarter.format(data.amt)}
+                  <span className="perc">{calcPerc(data.amt)}%</span>
+                </span>
+              </li>
+            )
+        )}
       </ul>
     </div>
   );
