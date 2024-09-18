9cc7f419c4e8e8dc3f398f26b7bb175031fae048
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-18 13:41:12 +0100

    Updated ListDisplay.js

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -1,13 +1,20 @@
 import IncomeList from "./IncomeList";
 import ExpenseList from "./ExpenseList";
 
-export default function ListDisplay({ incData, expData }) {
-  // console.log(incData);
-
-  return (
-    <div className="inc-exp-display">
-      <IncomeList incData={incData} />
-      <ExpenseList expData={expData} />
-    </div>
+export default function ListDisplay({ onDeleteAll, incData, expData }) {
+  return [...incData, ...expData].length ? (
+    <>
+      <div className="inc-exp-display">
+        <IncomeList incData={incData} />
+        <ExpenseList expData={expData} />
+      </div>
+      <div className="btn-div">
+        <button onClick={onDeleteAll} className="dlt-all-btn btn">
+          Delete All
+        </button>
+      </div>
+    </>
+  ) : (
+    <p className="init-msg">Input and income/expense to get started 😁</p>
   );
 }
