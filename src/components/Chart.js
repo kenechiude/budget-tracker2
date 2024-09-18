223f065bee0bbfe1a29e500343596dc0da96272d
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-18 13:39:36 +0100

    Updated Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,22 +1,28 @@
 export default function Chart({ inc, exp }) {
   const income = inc.reduce((acc, obj) => (acc += obj.amt), 0);
   const expense = exp.reduce((acc, obj) => (acc += obj.amt), 0);
+
+  const formarter = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    maximumFractionDigits: 2,
+  });
   const perc = Math.round((expense / income) * 100);
   const balance = income - expense;
 
   return (
     <div className="chart">
-      <span className="bal">{balance}</span>
+      <span className="bal">{formarter.format(balance)}</span>
 
       <div className="income">
         <span>Income</span>
-        <span>{income}</span>
+        <span>{formarter.format(income)}</span>
       </div>
 
       <div className="expense">
         <span>Expense</span>
         <span>
-          {expense} <span className="perc">{perc ? perc : 0}%</span>
+          {formarter.format(expense)}
+          <span className="perc">{perc ? perc : 0}%</span>
         </span>
       </div>
     </div>
